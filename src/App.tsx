ede3613479ed191664c19bae9427f0f0d83a8fcc
Jonathan Nilsson <koffeinmissbrukarn@gmail.com>
2023-02-20 10:08:50 +0100

    Adding todos now working

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import { useState } from "react";
 import React from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from './models/todo';
+import Todo from "./models/todo";
 
 function App() {
-const todos = [
-  new Todo('learn to be cool'),
-  new Todo('learn to be coolio')
-]
+  const [todos, setTodos] = useState<Todo[]>([]);
 
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((currentTodos) => {
+      return currentTodos.concat(newTodo);
+    });
+  };
+ 
   return (
     <div>
-      <NewTodo />
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos}></Todos>
     </div>
   );
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,26 +1,30 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
-const NewTodo = () => {
-
-/// Initialize Refs to fetch data from the form    
-const todoTextInputRef = useRef<HTMLInputElement>(null);
-
-    const submitHandler = (e: React.FormEvent ) => {
-e.preventDefault();
-
-const inputText = todoTextInputRef.current!.value
-
-if (inputText.trim().length === 0) {
-    /// Make errorhandler later.
-    return
+interface IAddTodo {
+  onAddTodo: (text: string) => void;
 }
 
+const NewTodo = (props: IAddTodo) => {
+  /// Initialize Refs to fetch data from the form
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const inputText = todoTextInputRef.current!.value;
+
+    if (inputText.trim().length === 0) {
+      /// Make errorhandler later.
+      return;
     }
+
+    props.onAddTodo(inputText);
+  };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Type Todo:</label>
-      <input type="text" id="text" ref={todoTextInputRef}/>
+      <input type="text" id="text" ref={todoTextInputRef} />
       <button>Add Todo</button>
     </form>
   );
